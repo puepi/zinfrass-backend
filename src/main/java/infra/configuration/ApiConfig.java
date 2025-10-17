08a52dd4222338e6efdbc5099dbe1ebd65fd2950
puepi <pmandel0001@yahoo.fr>
2025-10-17 13:39:26 +0100

    changes

--- src/main/java/infra/configuration/ApiConfig.java
+++ src/main/java/infra/configuration/ApiConfig.java
@@ -2,8 +2,16 @@ package infra.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ApiConfig {
@@ -18,5 +26,33 @@ public class ApiConfig {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/public/**").permitAll() // allow access to public path
+                .anyRequest().authenticated()) // any other request must be authenticated
+//                .formLogin(withDefaults()) // enable form login with   default page
+                .httpBasic(withDefaults())  // enable http basic authentication
+                .logout(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
